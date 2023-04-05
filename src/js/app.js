d36a6006abb86465627a1290604ddcc9b09aeca5
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2023-04-05 18:20:45 +0100

    Finish Fronted

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    eventListener();
+    darkMode();
+});
+
+function eventListener() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    mobileMenu.addEventListener("click", function() {
+        navegacionResponsive();
+    });
+};
+
+function navegacionResponsive() {
+    const navegacion =  document.querySelector(".navegacion");
+    navegacion.classList.toggle("mostrar");
+}
+
+function darkMode() {
+    const preferDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    
+    if(preferDarkMode.matches) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    };
+    
+    preferDarkMode.addEventListener("change", function() {
+        if(preferDarkMode.matches) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        };
+    })
+};
