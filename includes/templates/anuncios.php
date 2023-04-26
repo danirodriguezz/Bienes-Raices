01dd2a7c9856e4598777bb9e591d3ffcfcc53edb
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2023-04-26 09:59:02 +0100

    Add max-width to anuncios-descripcion

--- includes/templates/anuncios.php
+++ includes/templates/anuncios.php
@@ -21,7 +21,7 @@
         
         <div class="contenido-anuncio">
             <h3><?php echo $propiedad["titulo"];?></h3>
-            <p><?php echo $propiedad["descripcion"];?></p>
+            <p class="contenido-anuncio-descripcion"><?php echo $propiedad["descripcion"];?></p>
             <p class="precio">$ <?php echo $propiedad["precio"];?></p>
             <ul class="iconos-caracteristicas">
                 <li>
